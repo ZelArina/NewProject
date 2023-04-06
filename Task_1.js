6db202f8c603e57d6055e8c957caa795bc256607
Arina <zeleninaarina33@yandex.ru>
2023-04-06 21:39:14 +0200

    add cycles and conditions

--- Task_1.js
+++ Task_1.js
@@ -1,8 +1,5 @@
-const NumberOfFilms = +prompt("Сколько фильмов вы уже посмотрели?", "default"),
-MoviesName1 = prompt("Один из последних просмотренных фильмов","default"),
-Feedback1 = prompt("Насколько оцените его?","default"),
-MoviesName2 = prompt("Один из последних просмотренных фильмов","default"),
-Feedback2 = prompt("Насколько оцените его?","default");
+const NumberOfFilms = +prompt("Сколько фильмов вы уже посмотрели?", "");
+
 let PersonalMovieDB = {
     count: NumberOfFilms,
     movies: {},
@@ -11,7 +8,29 @@ let PersonalMovieDB = {
     privat: false
 };
 
-PersonalMovieDB.movies[MoviesName1] = Feedback1;
-PersonalMovieDB.movies[MoviesName2] = Feedback2;
+if (PersonalMovieDB.count < 10){
+    alert('Просмотрено довольно мало фильмов');
+}else if(PersonalMovieDB.count >= 10 && PersonalMovieDB.count < 30 ){
+    alert('Вы классический зритель');
+}else if(PersonalMovieDB.count >= 30){
+    alert('Вы киноман');
+}else{
+    alert('Произошла ошибка');
+}
+
+for(let i = 0; i < 2; i++){
+    let MoviesName = prompt("Один из последних просмотренных фильмов",""),
+    Feedback = prompt("Насколько оцените его?","");
+    if(MoviesName === null || MoviesName.length == 0 || MoviesName.length > 50){
+        i--;
+        continue;
+    }else if(Feedback === null|| Feedback.length == 0){
+        i--;
+        continue;
+    }else{
+        PersonalMovieDB.movies[MoviesName] = Feedback;
+    }
+}
+    
 
 console.log(PersonalMovieDB.movies);
